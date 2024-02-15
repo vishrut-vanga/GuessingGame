4cde1c6dfd3ea458619f358ed2d0756ca3b69b0e
vishrut-vanga <122230726+vishrut-vanga@users.noreply.github.com>
2024-02-15 12:20:09 -0500

    Add files via upload

---
+++ guessinggameoutput.js
@@ -0,0 +1,11 @@
+let { testNumber } = require("./guessinggame.js");
+
+process.stdout.write(
+  'I\'m thinking of a number from 1 through 10. What do you think it is? \n(Write "quit" to give up.)\n\nIs the number ... '
+);
+
+let playGame = (userInput) => {
+  let input = userInput.toString().trim();
+  testNumber(input);
+};
+process.stdin.on("data", playGame);
